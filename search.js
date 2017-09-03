eb56a79a4cea7d46acb7e4fb5c873d4302c0023b
@snowman <jimmoffitt@yahoo.com>
2017-09-02 22:54:12 -0600

    Update search.js

--- search.js
+++ search.js
@@ -1,20 +1,10 @@
-var nconf = require('nconf')
 var request = require("request")
 
-// load config
-nconf.file({ file: 'config.json' }).env()
-
-// twitter authentication
-#consumer_key = process.env.CONSUMER_KEY
-#consumer_secret = process.env.CONSUMER_SECRET
-#token = process.env.ACCESS_TOKEN
-#token_secret = process.env.ACCESS_TOKEN_SECRET
-
 var twitter_oauth = {
-  consumer_key: nconf.get('CONSUMER_KEY'),
-  consumer_secret: nconf.get('CONSUMER_SECRET'),
-  token: nconf.get('ACCESS_TOKEN'),
-  token_secret: nconf.get('ACCESS_TOKEN_SECRET')
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  token: process.env.ACCESS_TOKEN,
+  token_secret: process.env.ACCESS_TOKEN_SECRET
 }
 
 // build request
