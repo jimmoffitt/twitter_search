ba7abe86660583217482c80c4407dc5f506c99c7
@snowman <jimmoffitt@yahoo.com>
2017-09-06 22:41:35 -0600

    Update search.js

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 var request = require("request")
 
-//Twitter OAuth 
+//Twitter OAuth
 var search_auth = {
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
@@ -16,30 +16,31 @@ var search_config = {
 
 // build request <-- input
 var query = {
-  "query": "snow"
+  "query": "%28snow%20OR%20rain%29%20has%3Amedia"
 }
-
-//@@domain_main
-//@@product
 
 // request options
 var request_options = {
   //POST form with "body: query" below
-  //url: 'https://' + search_config['url'] + search_config['env'] + '.json', 
+  //url: 'https://' + search_config['url'] + search_config['env'] + '.json',
   //GET form with query included in URL.
-  url: 'https://' + search_config['url'] + search_config['env'] + '.json?query=' + query['query'], 
-  
+  url: 'https://' + search_config['url'] + search_config['env'] + '.json?query=' + query['query'],
+
   oauth: search_auth,
   json: true,
   headers: {
     'content-type': 'application/json'
   },
-  //body: query
+  //body: query //Uncomment with POST requests.
 }
 
 // POST request
-request.post(request_options, function (error, response, body) {
-  
+//request.post(request_options, function (error, response, body) {
+//GET request
+request(request_options, function (error, response, body) {
+
+  console.log(request_options['url'])
+
   if (error) {
     console.log('Error making search request.')
     console.log(error)
